630f85fb204441f2ee167911473f5a8573d9ec95
Marcus Aguirre <marcus.a.aguirre@gmail.com>
2019-03-19 15:18:07 -0500

    finished work

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,22 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        int myFavoriteNumber = 1;
+
+        String myString = "Hello";
+        System.out.println(myString);
+
+        long myNumber = 123;
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        String theNumberThree = "three";
+        int three = 12;
+
+
+        System.out.println(three);
 
     }
 
