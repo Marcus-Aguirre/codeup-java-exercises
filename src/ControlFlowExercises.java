00e38fba017a43f0e960793f647a09725c7859b5
Marcus Aguirre <marcus.a.aguirre@gmail.com>
2019-03-20 14:34:38 -0500

    completed work

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,127 @@
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+
+    public static void main(String[] args) {
+
+
+        /////////// WHILE LOOP ///////////
+
+        int whileFive = 5;
+
+        while (whileFive <= 6) {
+            System.out.println("WhileFive is " + whileFive);
+            whileFive++;
+        }
+
+
+
+        /////////// DO-WHILE LOOP ///////////
+
+        int doWhile = 0;
+
+        do {
+            System.out.println("The current doWhile is: " + doWhile);
+            doWhile += 2;
+        } while (doWhile <= 2);
+
+
+
+        /////////// FOR LOOP ///////////
+
+        for(int forNumber = 0; forNumber <= 1; forNumber++){
+
+            System.out.println("The forNumber is: " + forNumber);
+
+        }
+
+
+
+        /////////// FIZZ BUZZ / FOR LOOP ///////////
+
+        for(int fizzbuzz = 1; fizzbuzz <= 50; fizzbuzz++){
+
+            if(fizzbuzz % 15 == 0){
+
+                System.out.println("FIZZ-BUZZ");
+
+            }else if(fizzbuzz % 5 == 0){
+
+                System.out.println("BUZZ");
+
+            }else if(fizzbuzz % 3 == 0){
+
+                System.out.println("FIZZ");
+
+            }else{
+
+                System.out.println(fizzbuzz);
+
+            }
+
+
+        }
+
+
+        /////////////// TABLE OF POWERS ////////////////
+
+        Scanner tableTo = new Scanner(System.in);
+
+        System.out.print("Choose a number: ");
+
+        int numberTo = tableTo.nextInt();
+
+
+        System.out.println("\nHere is your table!!\n");
+
+        System.out.println("   number   |   squared   |   cubed   ");
+        System.out.println("--------------------------------------");
+
+        int numberUp = 0;
+
+        for(int i = numberTo; i > 0; i--){
+            numberUp++;
+            System.out.println("     " + (numberUp)  + "      |       " + (numberUp * numberUp) + "     |    " + (numberUp * numberUp * numberUp) + "     ");
+        }
+
+
+
+
+        /////////////// NUMERICAL GRADE CONVERTER ////////////////
+
+        System.out.println("Enter a numerical grade from 0 - 100: ");
+
+        Scanner numberToGrade = new Scanner(System.in);
+
+        int numericalGrade = numberToGrade.nextInt();
+
+
+        if(numericalGrade >= 88){
+
+            System.out.println("The score is an \"A\".");
+
+        }else if(numericalGrade >= 80 & numericalGrade < 87){
+
+            System.out.println("The score is an \"B\".");
+
+        }else if(numericalGrade >= 67 & numericalGrade < 79){
+
+            System.out.println("The score is an \"C\".");
+
+        }else if(numericalGrade >= 60 & numericalGrade < 67){
+
+            System.out.println("The score is an \"D\".");
+
+        }else if(numericalGrade <= 59){
+
+            System.out.println("The score is an \"F\".");
+
+        }
+
+
+
+
+
+    }
+
+}
