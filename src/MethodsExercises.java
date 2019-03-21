9c5faea6e8493669d26215f2851f86a470ccb00c
Marcus Aguirre <marcus.a.aguirre@gmail.com>
2019-03-21 11:25:54 -0500

    completed work on Methods Exercises

---
+++ src/MethodsExercises.java
@@ -0,0 +1,56 @@
+public class MethodsExercises {
+
+    public static int addition (int a, int b){
+
+        return a + b;
+
+    }
+
+    public static int subtraction (int a, int b){
+
+        return a - b;
+
+    }
+
+    public static int multiplication (int a, int b){
+
+        return a * b;
+
+    }
+
+    public static int division (int a, int b){
+
+        return a / b;
+
+    }
+
+    public static int modulus (int a, int b){
+
+        return a % b;
+
+    }
+
+
+
+
+    public static void main (String[] args){
+
+        System.out.println(" 5 + 10 = " + addition(5,10));
+
+        System.out.println(" 10 - 5 = " + subtraction(10,5));
+
+        System.out.println(" 5 * 10 = " + multiplication(5,10));
+
+        System.out.println(" 10 / 5 = " + division(10,5));
+
+        System.out.println(" 10 % 5 = " + modulus(10,5));
+
+    }
+
+
+
+
+
+
+
+}
