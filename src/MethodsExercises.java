87f162368b3bf582fc7197005bb51a2833b58ecb
Marcus Aguirre <marcus.a.aguirre@gmail.com>
2019-03-23 19:50:07 -0500

    added code to methods exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
 
     public static int addition (int a, int b){
@@ -30,6 +32,72 @@ public class MethodsExercises {
 
     }
 
+    public static int getInteger(int min, int max){
+
+        System.out.format("Enter a number between %s and %s: ", min, max);
+
+        Scanner getIntInput = new Scanner(System.in);
+
+        int userInt = getIntInput.nextInt();
+
+        boolean valid = false;
+
+       do{
+
+           if (userInt > max || userInt < min) {
+
+               System.out.println("Your chosen integer isn't in valid range.\nPlease try again: ");
+
+               userInt = getIntInput.nextInt();
+
+               valid = false;
+
+           } else {
+
+               valid = true;
+
+           }
+
+       }while(!valid);
+
+        return userInt;
+
+    }
+
+    public static void factorial(int num){
+
+        for(int i = 1; i <= num; i++){
+
+            long factorSolution = 1;
+
+            String message = "" + i + "! = ";
+
+
+            for(int x = 1; x <= i; x++){
+
+                if(i == x){
+
+                    factorSolution *= x;
+
+                    message += x;
+
+                    continue;
+                }
+
+                factorSolution *= x;
+
+                message += x + " x ";
+
+            }
+
+            message += " = " + factorSolution;
+
+            System.out.println(message);
+
+        }
+    }
+
+
 
 
     public static void main (String[] args){
@@ -43,6 +111,10 @@ public class MethodsExercises {
         System.out.println(" 10 / 5 = " + division(10,5));
 
         System.out.println(" 10 % 5 = " + modulus(10,5));
+
+       int newGuess = getInteger(5, 15);
+
+       factorial(5);
 
     }
 
