56a39c2eb79a7d8168fe6fcf1c46072f9122bdb9
Marcus Aguirre <marcus.a.aguirre@gmail.com>
2019-03-26 11:24:38 -0500

    created Shapes Package and reworked Circle exercise

--- src/Shapes/Circle.java
+++ src/Shapes/Circle.java
@@ -1,20 +1,38 @@
+package Shapes;
+
 public class Circle {
 
     private double radius;
+    private static double pi = 3.141592;
 
     public Circle(double radius){
+
         this.radius = radius;
+
     }
 
     public double getArea(){
+
+        return pi * (radius * radius);
 
     }
 
     public double getCircumference(){
 
+        return 2 * pi * radius;
 
     }
 
+
+
+
+    public static void main(String[] args) {
+        Circle square = new Circle(5);
+
+        System.out.println(square.getArea());
+        System.out.println(square.getCircumference());
+
+    }
 
 
 
