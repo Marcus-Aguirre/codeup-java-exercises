0d83e1f37727658afb878e0fedec6f4348b04253
Marcus Aguirre <marcus.a.aguirre@gmail.com>
2019-03-26 11:23:19 -0500

    completed Circle exercise

---
+++ src/Shapes/Circle.java
@@ -0,0 +1,22 @@
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public double getArea(){
+
+    }
+
+    public double getCircumference(){
+
+
+    }
+
+
+
+
+
+}
