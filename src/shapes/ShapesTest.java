60cbf1c041ae10d9c919e2c1f5d56a7b4afe46a3
Marcus Aguirre <marcus.a.aguirre@gmail.com>
2019-03-29 11:48:34 -0500

    completed shapes test

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -4,7 +4,9 @@ public class ShapesTest {
 
 
     public static void main(String[] args) {
+        Measurable myShape = new Rectangle(5, 10);
 
+        System.out.println(myShape.getArea());
 
 
 
